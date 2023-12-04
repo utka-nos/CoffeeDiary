a0ab92d34d9f259870447f44d9451e41f75fbd7b
dimikmusik@yandex.ru <dimikmusik@yandex.ru>
2023-12-05 00:21:18 +0300

    [*] переработана логика юнит теста для CoffeeControllerTest

--- coffee-service/src/test/java/com/example/api/coffee/controller/CoffeeControllerTest.java
+++ coffee-service/src/test/java/com/example/api/coffee/controller/CoffeeControllerTest.java
@@ -3,26 +3,31 @@ package com.example.api.coffee.controller;
 import com.example.api.coffee.service.CoffeeService;
 import com.example.dto.CoffeeDTO;
 import com.example.jsonviews.CoffeeJSONView;
-import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.example.config.Role;
+import org.example.config.JwtContext;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.json.JsonTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
-import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.security.test.context.support.WithSecurityContext;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.core.oidc.StandardClaimNames;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-@SpringBootTest
+import java.util.List;
+
+//@SpringBootTest - поднимает ВЕСЬ контекст, не подходит нам, это не интеграционный тест
+@WebMvcTest(controllers = CoffeeController.class)  // поднимает только, контекст связанный с указанным контроллером
+//TODO: подумать как при добавлении этого импорта избавиться от проблемы с авторизацией jwt токена без обращения в сервис авторизации
+@Import({JwtContext.class}) // импортируем настройкаи JWTConfig чтобы проверить авторизацию по jwt токену
 @ActiveProfiles(profiles = {"test"})
 @AutoConfigureMockMvc
 class CoffeeControllerTest {
@@ -49,7 +54,7 @@ class CoffeeControllerTest {
     }
 
     @Test
-    @WithMockUser(authorities = {"USER"}, roles = "USER")
+    //@WithMockUser(authorities = {"USER"})
     void addNewCoffee() throws Exception {
         CoffeeDTO coffeeDTO = new CoffeeDTO();
         coffeeDTO.setName("brazil");
@@ -59,9 +64,24 @@ class CoffeeControllerTest {
         coffeeDTO.setId(1L);
 
         String ex = objectMapper.writerWithView(CoffeeJSONView.Main.class).forType(CoffeeDTO.class).writeValueAsString(coffeeDTO);
-        mockMvc.perform(MockMvcRequestBuilders.post("/coffee").content(s).contentType(MediaType.APPLICATION_JSON))
+        mockMvc.perform(
+                MockMvcRequestBuilders
+                        .post("/coffee")
+                        .with(
+                                SecurityMockMvcRequestPostProcessors
+                                        .jwt()
+                                        .authorities(
+                                                List.of(new SimpleGrantedAuthority("USER"))
+                                        )
+                                        .jwt(jwt ->
+                                                jwt.claim(StandardClaimNames.PREFERRED_USERNAME, "dima")
+                                        )
+                        )
+                        .content(s).contentType(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.status().is(201))
                 .andExpect(MockMvcResultMatchers.content().string(ex));
+
+        //Mockito.verify(coffeeService, Mockito.times(1)).addNewCoffee(coffeeDTO);
 
     }
 
